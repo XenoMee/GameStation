2e8707283df27b6295827795fe82fa6bcac0f0b3
Adrian Trandafir <trandafiradrian98@gmail.com>
2024-03-13 22:30:12 +0200

    update post likes number

--- utilities/postsData.js
+++ utilities/postsData.js
@@ -7,7 +7,7 @@ export const postsArray = [
     photo: "./assets/images/post/PC-Plant.jpg",
     comment:
       "Chillin' with my trusty sidekicks: my PC and my plant babies 🌿💻 There's something oddly satisfying about the mix of tech and greenery in my space. Got my setup ready for a day of productivity and maybe a little gaming break in between watering sessions.",
-    likes: 121,
+    likes: 190,
     hastags: "#TechAndNature #WorkspaceVibes",
   },
   {
@@ -29,7 +29,7 @@ export const postsArray = [
     photo: "./assets/images/post/PC-Green.jpg",
     comment:
       "Meet my trusty companion for late-night coding sessions and gaming marathons! Who needs sleep when you have a setup like this? 😎💻",
-    likes: 121,
+    likes: 38,
     hastags: "#CodingLife #Gamer",
   },
   {
@@ -40,7 +40,7 @@ export const postsArray = [
     photo: "./assets/images/post/PC-Cat.jpg",
     comment:
       "No better feeling than coming home to my beloved cat and PC setup after a long day 🏠🐈💻",
-    likes: 121,
+    likes: 240,
     hastags: "#HomeSweetHome",
   },
   {
@@ -51,7 +51,7 @@ export const postsArray = [
     photo: "./assets/images/post/PC-PurpleTeal.jpg",
     comment:
       "Just upgraded my setup with some RGB goodness! Who needs sunlight when you have these neon lights? 🌈✨",
-    likes: 121,
+    likes: 378,
     hastags: "#PCGaming",
   },
   {
@@ -62,7 +62,7 @@ export const postsArray = [
     photo: "./assets/images/post/PC-White.jpg",
     comment:
       "Just a casual snapshot of my cozy corner 🖥️! Work, play, and everything in between happens right here. Who else feels like their PC is an extension of themselves?",
-    likes: 121,
+    likes: 88,
     hastags: "#CozySetup #HomeOffice",
   },
   {
@@ -73,7 +73,7 @@ export const postsArray = [
     photo: "./assets/images/post/PC-Blue.jpg",
     comment:
       "New setup vibes! Just leveled up my PC game and couldn't resist showing it off 🚀 Loving these icy cold lights giving the whole room that futuristic feel ❄️ Ready to dive into some serious gaming sessions now! 💻🎮",
-    likes: 121,
+    likes: 150,
     hastags: "#PCMasterRace #ColdLights #GamingSetup",
   },
   {
@@ -84,7 +84,7 @@ export const postsArray = [
     photo: "./assets/images/post/PC-DarkBG.jpg",
     comment:
       "Geeking out over my latest tecuseh addition 🤓! This beast of a machine is ready to tackle anything I throw at it. Let's see what kind of creative projects we can cook up together!",
-    likes: 121,
+    likes: 179,
     hastags: "#TechEnthusiast #CreativeMinds",
   },
 ];
