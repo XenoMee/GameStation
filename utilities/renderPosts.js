81db68de7d7c2e2da4da8c0b04abd56e8a847936
Adrian Trandafir <trandafiradrian98@gmail.com>
2024-03-13 14:30:49 +0200

    refactor code to render the created post in one line

--- utilities/renderPosts.js
+++ utilities/renderPosts.js
@@ -1,20 +1,10 @@
 import { postsArray } from "./postsData.js";
-import * as postBody from "./createPost.js";
+import { createPost } from "./createPost.js";
 
 export const renderPosts = () => {
   const postsContainerEl = document.querySelector(".feed");
 
   postsArray.forEach((post) => {
-    const postContainer = document.createElement("div");
-    postContainer.className = "post bg-white";
-
-    postContainer.innerHTML += `
-      ${postBody.createPostHeader(post)}
-      ${postBody.createPostPhoto(post)}
-      ${postBody.createPostButtons(post)}
-      ${postBody.createPostDescription(post)}
-    `;
-
-    postsContainerEl.appendChild(postContainer);
+    postsContainerEl.appendChild(createPost(post));
   });
 };
