0a9f7756ffdde24004854cfe71ae20b9ba6d4bba
Adrian Trandafir <trandafiradrian98@gmail.com>
2024-03-13 14:41:34 +0200

    get hold of feed container element globally

--- utilities/renderPosts.js
+++ utilities/renderPosts.js
@@ -1,9 +1,8 @@
 import { postsArray } from "./postsData.js";
 import { createPost } from "./createPost.js";
+const postsContainerEl = document.querySelector(".feed");
 
-export const renderPosts = () => {
-  const postsContainerEl = document.querySelector(".feed");
-
+export const renderPosts = (e) => {
   postsArray.forEach((post) => {
     postsContainerEl.appendChild(createPost(post));
   });
