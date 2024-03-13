d1456f215ad8e07a5bd33d4913f7c7c41c45596e
Adrian Trandafir <trandafiradrian98@gmail.com>
2024-03-13 21:57:59 +0200

    create user interaction to like and dislike post

---
+++ utilities/likePost.js
@@ -0,0 +1,39 @@
+import { postsContainerEl } from "./renderPosts.js";
+
+export const handlePostLike = () => {
+  const getPostClick = (e) => {
+    const postEl = e.target.closest(".post");
+    const likeEl = e.target.closest("[data-action='like']");
+    if (!postEl) return;
+
+    if (likeEl) updateLikeStatus(postEl);
+  };
+
+  postsContainerEl.addEventListener("click", getPostClick);
+
+  const updateLikeStatus = (post) => {
+    const likeBtn = post.querySelector(".btn-like");
+    const heartIcon = likeBtn.querySelector(".icon-heart");
+    const postLikes = post.querySelector(".post-likes");
+    let likeCount = { value: Number(postLikes.textContent) };
+    let isLiked = post.getAttribute("data-liked") === "true";
+
+    isLiked
+      ? dislikePost(likeBtn, heartIcon, likeCount)
+      : likePost(likeBtn, heartIcon, likeCount);
+
+    post.setAttribute("data-liked", String(!isLiked));
+    postLikes.textContent = likeCount.value;
+  };
+
+  const dislikePost = (button, icon, likesNumber) => {
+    button.classList.remove("text-red-500");
+    icon.classList.remove("fill-red-500");
+    likesNumber.value--;
+  };
+  const likePost = (button, icon, likesNumber) => {
+    button.classList.add("text-red-500");
+    icon.classList.add("fill-red-500");
+    likesNumber.value++;
+  };
+};
