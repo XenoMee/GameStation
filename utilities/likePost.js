8f78be5ed62d3a3062e73eeeeb07d936553048e9
Adrian Trandafir <trandafiradrian98@gmail.com>
2024-03-18 17:08:22 +0200

    fix issue with liking post

--- utilities/likePost.js
+++ utilities/likePost.js
@@ -1,42 +1,42 @@
-import { postsContainerEl } from "./renderPosts.js";
+import { postsContainerEl } from './renderPosts.js';
 
 export const handlePostLike = () => {
   const getPostClick = (e) => {
-    const postEl = postsContainerEl.querySelector(".post");
-    const likeEl = e.target.parentElement.dataset.action;
+    const postEl = e.target.closest('.post');
+    const likeEl = e.target.closest("[data-action='like']");
     if (!postEl) return;
 
     if (likeEl) updateLikeStatus(postEl);
   };
 
-  postsContainerEl.addEventListener("click", getPostClick);
+  postsContainerEl.addEventListener('click', getPostClick);
 
   const updateLikeStatus = (post) => {
-    const likeBtn = post.querySelector(".btn-like");
-    const heartIcon = likeBtn.querySelector(".fa-heart");
+    const likeBtn = post.querySelector('.btn-like');
+    const heartIcon = likeBtn.querySelector('.fa-heart');
     console.log(heartIcon);
-    const postLikes = post.querySelector(".post-likes");
+    const postLikes = post.querySelector('.post-likes');
     let likeCount = { value: Number(postLikes.textContent) };
-    let isLiked = post.getAttribute("data-liked") === "true";
+    let isLiked = post.getAttribute('data-liked') === 'true';
 
     isLiked
       ? dislikePost(likeBtn, heartIcon, likeCount)
       : likePost(likeBtn, heartIcon, likeCount);
 
-    post.setAttribute("data-liked", String(!isLiked));
+    post.setAttribute('data-liked', String(!isLiked));
     postLikes.textContent = likeCount.value;
   };
 
   const dislikePost = (button, icon, likesNumber) => {
-    button.classList.remove("text-red-500");
-    button.classList.add("hover:opacity-50");
-    icon.classList.remove("fa-solid");
+    button.classList.remove('text-red-500');
+    button.classList.add('hover:opacity-50');
+    icon.classList.remove('fa-solid');
     likesNumber.value--;
   };
   const likePost = (button, icon, likesNumber) => {
-    button.classList.add("text-red-500");
-    button.classList.remove("hover:opacity-50");
-    icon.classList.add("fa-solid");
+    button.classList.add('text-red-500');
+    button.classList.remove('hover:opacity-50');
+    icon.classList.add('fa-solid');
     likesNumber.value++;
   };
 };
